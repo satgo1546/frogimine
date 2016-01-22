45364ac229e55fcbaa6be04a54ed996f66cf07aa
bobcao3 <bobcaocheng@163.com>
2016-01-22 16:00:44 +0800

    modified:   Drivers/Basic/i686/memory.c -> New functions of paging 	modified:   Drivers/Basic/i686/memory.h -> New functions of paging

--- Drivers/Basic/i686/memory.c
+++ Drivers/Basic/i686/memory.c
@@ -9,6 +9,27 @@
 uint32_t volatile page_directory[1024] __attribute__((aligned(4096)));
 uint32_t volatile first_page_table[1024] __attribute__((aligned(4096)));
 
+//---------------------------------------------------------------------------
+// ● 将页表绑定入页目录
+//---------------------------------------------------------------------------
+void map_pages_to_dir(int page_id, uint32_t* page_tab, uint8_t flag) {
+	page_directory[page_id] = ((uint32_t)page_tab) | flag;
+}
+
+//---------------------------------------------------------------------------
+// ● 将内存绑定入页表 (KByte计数)
+//---------------------------------------------------------------------------
+void map_physical_to_page_tab(uint32_t* page_tab, uint8_t flag, uint32_t f, uint32_t t) {
+	for(uint32_t i = f; i < t; i++)
+	{
+		page_tab[i] = (i * 0x1000) | flag; 
+	}
+}
+
+
+//---------------------------------------------------------------------------
+// ● 内存初始化
+//---------------------------------------------------------------------------
 void memory_init () {
 	
 	// Init paging ===================================================
@@ -25,8 +46,9 @@ void memory_init () {
 	
 	// holds the physical address where we want to start mapping these pages to.
 	// in this case, we want to map these pages to the very beginning of memory.
-	// we will fill 512 entries in the table, mapping 2 megabytes
-	for(unsigned int i = 0; i < 1024; i++)
+	// we will fill 512 entries in the table, mapping 1.5 megabytes
+	// (lower memory 1M, kernel 512KByte)
+	for(uint32_t i = 0; i < 384; i++)
 	{
 		// As the address is page aligned, it will always leave 12 bits zeroed.
 		// Those bits are used by the attributes ;)
@@ -35,7 +57,7 @@ void memory_init () {
 	}
 	
 	// Put the Page Table in the Page Directory
-	page_directory[0] = ((unsigned int)first_page_table) | 3;
+	map_pages_to_dir(0, (uint32_t*)first_page_table, 3);
 	
 	// enable it
 	asm volatile ("movl %%eax, %%cr3" :: "a" (&page_directory)); // load PDPT into CR3
--- Drivers/Basic/i686/memory.h
+++ Drivers/Basic/i686/memory.h
@@ -9,6 +9,8 @@
 #ifndef INCLUDE_MEMORY_H_
 #define INCLUDE_MEMORY_H_
 
+void map_pages_to_dir(int page_id, uint32_t* page_tab, uint8_t flag);
+void map_physical_to_page_tab(uint32_t* page_tab, uint8_t flag, uint32_t f, uint32_t t);
 void memory_init();
 
 #endif
