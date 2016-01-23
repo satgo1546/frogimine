57c962445a9cf95a8e912756dcd2bca29e10ebc4
bobcao3 <bobcaocheng@163.com>
2016-01-23 14:44:55 +0800

    增加读取Multiboot结构，基本的物理内存管理

--- Drivers/Basic/i686/memory.cpp
+++ Drivers/Basic/i686/memory.cpp
@@ -14,20 +14,44 @@ void Memory::map_pages_to_dir(int page_id, uint32_t* page_tab, uint8_t flag) {
 }
 
 //---------------------------------------------------------------------------
-// ● 将内存绑定入页表 (KByte计数)
+// ● 占有物理内存(页计数)
 //---------------------------------------------------------------------------
-void Memory::map_physical_to_page_tab(uint32_t* page_tab, uint8_t flag, uint32_t f, uint32_t t) {
-	for(uint32_t i = f; i < t; i++)
-	{
-		page_tab[i] = (i * 0x1000) | flag; 
+void Memory::AllocPhy(uint32_t f, uint32_t t) {
+	for(uint32_t i = f; i < t; i++) {
+		*(phy_c + i) = TRUE;
 	}
 }
 
+//---------------------------------------------------------------------------
+// ● 释放物理内存(页计数)
+//---------------------------------------------------------------------------
+void Memory::ReleasePhy(uint32_t f, uint32_t t) {
+	for(uint32_t i = f; i < t; i++) {
+		*(phy_c + i) = FALSE;
+	}
+}
+
+//---------------------------------------------------------------------------
+// ● 将内存绑定入页表 (页计数)
+//---------------------------------------------------------------------------
+void Memory::map_physical_to_page_tab(uint32_t* page_tab, uint8_t flag, uint32_t f, uint32_t t) {
+	for(uint32_t i = f; i < t; i++) {
+		page_tab[i] = (i * 0x1000) | flag; 
+	}
+	AllocPhy(f,t);
+}
+
+extern multiboot_t *glb_mboot_ptr;
 
 //---------------------------------------------------------------------------
 // ● 内存初始化
 //---------------------------------------------------------------------------
 Memory::Memory () {
+	
+	// Setup
+	upper_mem = glb_mboot_ptr->mem_upper;
+	mem_size = upper_mem + 1024;
+	phy_c = (bool*)0x120000;
 	
 	// Init paging ===================================================
 	//set each entry to not present
@@ -40,17 +64,19 @@ Memory::Memory () {
 		//   Not Present: The page table is not present
 		page_directory[j] = 0x00000002;
 	}
+	ReleasePhy(0, mem_size >> 2);
 	
 	// holds the physical address where we want to start mapping these pages to.
 	// in this case, we want to map these pages to the very beginning of memory.
 	// we will fill 512 entries in the table, mapping 1.5 megabytes
-	// (lower memory 1M, kernel 512KByte)
-	for(uint32_t i = 0; i < 384; i++)
+	// (lower memory 1M, kernel 128KByte, rest are for phy alloc)
+	for(uint32_t i = 0; i < (256 + 32 + (mem_size >> 14 ) + 1); i++)
 	{
 		// As the address is page aligned, it will always leave 12 bits zeroed.
 		// Those bits are used by the attributes ;)
-		kern_page_table[i] = (i * 0x1000) | SL_RW_P; 
+		kern_page_table[i] = (i * 0x1000) | SL_RW_P;
 	}
+	AllocPhy(256, 256 + 32 + (mem_size >> 14) + 1);
 	
 	// Put the Page Table in the Page Directory
 	map_pages_to_dir(0, (uint32_t*)kern_page_table, SL_RW_P);
