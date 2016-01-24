7d952d395192ae83ed99a0926b685f3ac54a1943
bobcao3 <bobcaocheng@163.com>
2016-01-24 18:33:27 +0800

    现在IRQ和ISR都能用了！

--- Drivers/i686/Basic/idt.h
+++ Drivers/i686/Basic/idt.h
@@ -23,9 +23,7 @@
 #include "StdC++/std.h"
 #include "Basic/io.h"
 
-extern "C" {
-extern void idt_update(uint32_t ptr);
-}
+
 
 class IDT {
 	public:
@@ -84,55 +82,77 @@ class IDT {
 	//---------------------------------------------------------------------------
 	interrupt_handler_t interrupt_handlers[256] __attribute__((packed));
 
+
+	public:
+	//---------------------------------------------------------------------------
+	// ● IDT门设定
+	//---------------------------------------------------------------------------
+	void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
+	//---------------------------------------------------------------------------
+	// ● IDT表设定
+	//---------------------------------------------------------------------------
+	void idt_set_gates();
+	//---------------------------------------------------------------------------
+	// ● IDT初始化
+	//---------------------------------------------------------------------------
+	IDT();
+};
+
+extern "C" {
+	extern void idt_update(uint32_t ptr);
 	//---------------------------------------------------------------------------
 	// ● 声明中断处理函数 0-19 属于 CPU 的异常中断
 	//---------------------------------------------------------------------------
 	// ISR:中断服务程序(interrupt service routine)
 	//---------------------------------------------------------------------------
-	static void isr0(); 		// 0 #DE 除 0 异常 
-	static void isr1(); 		// 1 #DB 调试异常 
-	static void isr2(); 		// 2 NMI 
-	static void isr3(); 		// 3 BP 断点异常 
-	static void isr4(); 		// 4 #OF 溢出 
-	static void isr5(); 		// 5 #BR 对数组的引用超出边界 
-	static void isr6(); 		// 6 #UD 无效或未定义的操作码 
-	static void isr7(); 		// 7 #NM 设备不可用(无数学协处理器) 
-	static void isr8(); 		// 8 #DF 双重故障(有错误代码) 
-	static void isr9(); 		// 9 协处理器跨段操作 
-	static void isr10(); 		// 10 #TS 无效TSS(有错误代码) 
-	static void isr11(); 		// 11 #NP 段不存在(有错误代码) 
-	static void isr12(); 		// 12 #SS 栈错误(有错误代码) 
-	static void isr13(); 		// 13 #GP 常规保护(有错误代码) 
-	static void isr14(); 		// 14 #PF 页故障(有错误代码) 
-	static void isr15(); 		// 15 CPU 保留 
-	static void isr16(); 		// 16 #MF 浮点处理单元错误 
-	static void isr17(); 		// 17 #AC 对齐检查 
-	static void isr18(); 		// 18 #MC 机器检查 
-	static void isr19(); 		// 19 #XM SIMD(单指令多数据)浮点异常
+	extern void isr0(); 		// 0 #DE 除 0 异常 
+	extern void isr1(); 		// 1 #DB 调试异常 
+	extern void isr2(); 		// 2 NMI 
+	extern void isr3(); 		// 3 BP 断点异常 
+	extern void isr4(); 		// 4 #OF 溢出 
+	extern void isr5(); 		// 5 #BR 对数组的引用超出边界 
+	extern void isr6(); 		// 6 #UD 无效或未定义的操作码 
+	extern void isr7(); 		// 7 #NM 设备不可用(无数学协处理器) 
+	extern void isr8(); 		// 8 #DF 双重故障(有错误代码) 
+	extern void isr9(); 		// 9 协处理器跨段操作 
+	extern void isr10(); 		// 10 #TS 无效TSS(有错误代码) 
+	extern void isr11(); 		// 11 #NP 段不存在(有错误代码) 
+	extern void isr12(); 		// 12 #SS 栈错误(有错误代码) 
+	extern void isr13(); 		// 13 #GP 常规保护(有错误代码) 
+	extern void isr14(); 		// 14 #PF 页故障(有错误代码) 
+	extern void isr15(); 		// 15 CPU 保留 
+	extern void isr16(); 		// 16 #MF 浮点处理单元错误 
+	extern void isr17(); 		// 17 #AC 对齐检查 
+	extern void isr18(); 		// 18 #MC 机器检查 
+	extern void isr19(); 		// 19 #XM SIMD(单指令多数据)浮点异常
 	//---------------------------------------------------------------------------
 	// 20-31 Intel 保留
 	//---------------------------------------------------------------------------
-	static void isr20();
-	static void isr21();
-	static void isr22();
-	static void isr23();
-	static void isr24();
-	static void isr25();
-	static void isr26();
-	static void isr27();
-	static void isr28();
-	static void isr29();
-	static void isr30();
-	static void isr31();
+	extern void isr20();
+	extern void isr21();
+	extern void isr22();
+	extern void isr23();
+	extern void isr24();
+	extern void isr25();
+	extern void isr26();
+	extern void isr27();
+	extern void isr28();
+	extern void isr29();
+	extern void isr30();
+	extern void isr31();
 	//---------------------------------------------------------------------------
 	// 32～255 用户自定义异常
 	//---------------------------------------------------------------------------
-	static void isr180();		// 180 系统中断（Syscall）
+	extern void isr180();		// 180 系统中断（Syscall）
 								// 不使用int80，int80用于兼容unix/linux syscall
+	//---------------------------------------------------------------------------
+	// ● isr处理函数
+	//---------------------------------------------------------------------------
+	extern void isr_handler(IDT::pt_regs *regs);
 	//---------------------------------------------------------------------------
 	// ● irq处理函数
 	//---------------------------------------------------------------------------
-	static void irq_handler(pt_regs *regs);
+	extern void irq_handler(IDT::pt_regs *regs);
 	//---------------------------------------------------------------------------
 	// 定义IRQ
 	//---------------------------------------------------------------------------
@@ -157,36 +177,22 @@ class IDT {
 	//---------------------------------------------------------------------------
 	// IRQ:中断请求(Interrupt Request)
 	//---------------------------------------------------------------------------
-	static void irq0();		// 电脑系统计时器
-	static void irq1(); 		// 键盘
-	static void irq2(); 		// 与 IRQ9 相接，MPU-401 MD 使用
-	static void irq3(); 		// 串口设备
-	static void irq4(); 		// 串口设备
-	static void irq5(); 		// 建议声卡使用
-	static void irq6(); 		// 软驱传输控制使用
-	static void irq7(); 		// 打印机传输控制使用
-	static void irq8(); 		// 即时时钟
-	static void irq9(); 		// 与 IRQ2 相接，可设定给其他硬件
-	static void irq10(); 		// 建议网卡使用
-	static void irq11(); 		// 建议 AGP 显卡使用
-	static void irq12(); 		// 接 PS/2 鼠标，也可设定给其他硬件
-	static void irq13(); 		// 协处理器使用
-	static void irq14(); 		// IDE0 传输控制使用
-	static void irq15(); 		// IDE1 传输控制使用
+	extern void irq0();		// 电脑系统计时器
+	extern void irq1(); 		// 键盘
+	extern void irq2(); 		// 与 IRQ9 相接，MPU-401 MD 使用
+	extern void irq3(); 		// 串口设备
+	extern void irq4(); 		// 串口设备
+	extern void irq5(); 		// 建议声卡使用
+	extern void irq6(); 		// 软驱传输控制使用
+	extern void irq7(); 		// 打印机传输控制使用
+	extern void irq8(); 		// 即时时钟
+	extern void irq9(); 		// 与 IRQ2 相接，可设定给其他硬件
+	extern void irq10(); 		// 建议网卡使用
+	extern void irq11(); 		// 建议 AGP 显卡使用
+	extern void irq12(); 		// 接 PS/2 鼠标，也可设定给其他硬件
+	extern void irq13(); 		// 协处理器使用
+	extern void irq14(); 		// IDE0 传输控制使用
+	extern void irq15(); 		// IDE1 传输控制使用
 
-	public:
-	//---------------------------------------------------------------------------
-	// ● IDT门设定
-	//---------------------------------------------------------------------------
-	void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags);
-	//---------------------------------------------------------------------------
-	// ● IDT表设定
-	//---------------------------------------------------------------------------
-	void idt_set_gates();
-	//---------------------------------------------------------------------------
-	// ● IDT初始化
-	//---------------------------------------------------------------------------
-	IDT();
-};
-
+}
 #endif
