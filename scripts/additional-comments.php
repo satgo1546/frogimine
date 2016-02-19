4b50347f11dece1b8dd1c0ec040b4b0ce5779506
Frog Chen <satgo1546@yeah.net>
2016-02-19 18:27:45 +0800

    添加scripts/additional-comments.php
    添加一个PHP脚本。

---
+++ scripts/additional-comments.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+#  Copyright 2016 Frog Chen
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#==============================================================================
+# ■ additional-comments.php
+#------------------------------------------------------------------------------
+#   额外的（通常是塞不进其它代码中的）注释。关于在一个C++项目中使用PHP的理由……
+#==============================================================================
+
+echo "PHP是世界上最好的语言！";
+
+# 现在还没有东西。以后除了注释也不会有任何东西。
+?>
